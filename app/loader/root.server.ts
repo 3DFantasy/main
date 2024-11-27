23bef335a36df98a625db41df6135b34ae629411
Wilson Birch <wilsonbirch@gmail.com>
2024-11-27 11:16:13 -0500

    WIP

--- app/loader/root.server.ts
+++ app/loader/root.server.ts
@@ -1,4 +1,4 @@
-import { gameFindMany } from '~/dao/index.server'
+import { gameFindMany, playFindMany, playFindUnique, playUpdate } from '~/dao/index.server'
 import { authenticator } from '~/utils/auth/auth.server'
 import { parsePlays } from '~/utils/parse/index.server'
 import {
@@ -14,9 +14,106 @@ import {
 } from '~/utils/puppeteer'
 
 import type { PXPAPIResponse } from '~/types'
+import { teamArray } from '~/data/teamArray'
 
 export const rootLoader = async (request: Request) => {
 	const account = await authenticator.isAuthenticated(request)
+
+	const points: number[] = []
+
+	const plays = await playFindMany({
+		where: {
+			type: 'Run',
+			down: null,
+			isScoring: false,
+			startPosition: '',
+		},
+	})
+
+	console.log(plays.length)
+
+	// plays.map(async (play) => {
+	// 	console.log('---------------------------')
+	// 	const teamAbr = teamArray.filter((team) => {
+	// 		return team.geniusTeamId === play.geniusTeamId
+	// 	})[0].abr
+	// 	// console.log(play.description)
+	// 	const previousPlay = await playFindUnique({
+	// 		where: {
+	// 			driveId: play.Drive.id,
+	// 			number: play.number - 1,
+	// 		},
+	// 	})
+	// 	const previousPlayYardLine = previousPlay[0].startPosition.slice(-2)
+	// 	const downNDistance = previousPlay[0].startPosition.includes('1st') ? '2nd & 1 at ' : '3rd & 1 at '
+	// 	if (previousPlay[0].startPosition.includes(teamAbr) && previousPlay[0].yardsGained) {
+	// 		const startPositionYardLine = Number(previousPlayYardLine) + previousPlay[0].yardsGained
+	// 		if (startPositionYardLine < 55) {
+	// 			const newStartPosition = downNDistance + teamAbr + ' ' + startPositionYardLine
+	// 			if (previousPlay[0].endPosition === '') {
+	// 				const previousPlayUpdates = await playUpdate({
+	// 					where: {
+	// 						id: previousPlay[0].id,
+	// 					},
+	// 					data: {
+	// 						endPosition: newStartPosition,
+	// 					},
+	// 				})
+	// 			}
+
+	// 			const updatedPlay = await playUpdate({
+	// 				where: {
+	// 					id: play.id,
+	// 				},
+	// 				data: {
+	// 					startPosition: newStartPosition,
+	// 					down: previousPlay[0].startPosition.includes('1st') ? 2 : 3,
+	// 					distance: '1',
+	// 					yardLine: -startPositionYardLine,
+	// 				},
+	// 			})
+	// 			console.log(updatedPlay.id)
+	// 		}
+	// 	} else if (!previousPlay[0].startPosition.includes(teamAbr) && previousPlay[0].yardsGained) {
+	// 		const startPositionYardLine = Number(previousPlayYardLine) - previousPlay[0].yardsGained
+	// 		const part1 = previousPlay[0].startPosition.slice(0, 11)
+	// 		const part2 = previousPlay[0].startPosition.slice(-2)
+
+	// 		const newStartPosition = previousPlay[0].startPosition
+	// 			.replace(part1, downNDistance)
+	// 			.replace(part2, startPositionYardLine.toString())
+
+	// 		if (previousPlay[0].endPosition === '') {
+	// 			const previousPlayUpdates = await playUpdate({
+	// 				where: {
+	// 					id: previousPlay[0].id,
+	// 				},
+	// 				data: {
+	// 					endPosition: newStartPosition,
+	// 				},
+	// 			})
+	// 		}
+
+	// 		const updatedPlay = await playUpdate({
+	// 			where: {
+	// 				id: play.id,
+	// 			},
+	// 			data: {
+	// 				startPosition: newStartPosition,
+	// 				down: previousPlay[0].startPosition.includes('1st') ? 2 : 3,
+	// 				distance: '1',
+	// 				yardLine: startPositionYardLine,
+	// 			},
+	// 		})
+	// 		console.log(updatedPlay.id)
+	// 	}
+	// })
+
+	// const sum = points.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+	// console.log((sum / plays.length).toFixed(2))
+
+	// console.log(plays.length)
 
 	// const games = await gameFindMany({
 	// 	where: {
