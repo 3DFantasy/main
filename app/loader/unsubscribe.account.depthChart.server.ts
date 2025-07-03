77ddff23f895a95bd0c6679c9adc9ad0bad29ca6
Wilson Birch <wilsonbirch@gmail.com>
2025-07-02 21:55:29 -0400

    unprotect unsubscribe route

--- app/loader/unsubscribe.account.depthChart.server.ts
+++ app/loader/unsubscribe.account.depthChart.server.ts
@@ -1,11 +1,9 @@
 import { LoaderFunctionArgs } from '@remix-run/node'
 import { db } from '~/lib/db.server'
-import { authenticator } from '~/utils/auth/auth.server'
 import { getTeamTitles } from '~/utils/index.server'
 import { parseUnsubscribeLoader } from '~/utils/parse/loaders/unsubscribe.server'
 
 import type { Account } from '@prisma/client'
-import type { AuthAccount } from '~/utils/auth/auth.server'
 import type { TeamTitleObj } from '~/utils/getTeamTitles.server'
 
 export type LoaderData = {
@@ -16,12 +14,7 @@ export type LoaderData = {
 }
 
 export const unsubscribeLoader = async (request: Request, params: LoaderFunctionArgs['params']) => {
-	const authAccount: AuthAccount = await authenticator.isAuthenticated(request, {
-		failureRedirect: `/auth/login?nextUrl=/unsubscribe/${params.account}/${params.depthChart}`,
-	})
-
 	const parsedParams = await parseUnsubscribeLoader({
-		account: authAccount,
 		params,
 	})
 
@@ -37,7 +30,7 @@ export const unsubscribeLoader = async (request: Request, params: LoaderFunction
 
 	const account = await db.account.findUnique({
 		where: {
-			id: authAccount.id,
+			uuid: parsedParams.value.accountUUID,
 		},
 	})
 
--- app/utils/parse/loaders/unsubscribe.server.ts
+++ app/utils/parse/loaders/unsubscribe.server.ts
@@ -3,29 +3,31 @@ import Result, { err, ok } from 'true-myth/result'
 import { db } from '~/lib/db.server'
 
 import type { Error } from '~/types'
-import type { AuthAccount } from '~/utils/auth/auth.server'
 
 export type ParseUnsubscribeLoaderOutput = { accountUUID: string; depthChartUUID: string }
 
 export const parseUnsubscribeLoader = async ({
-	account,
 	params,
 }: {
-	account: AuthAccount
 	params: LoaderFunctionArgs['params']
 }): Promise<Result<ParseUnsubscribeLoaderOutput, Error>> => {
+	const accountLookup = await db.account.findUnique({
+		where: {
+			uuid: params.account,
+		},
+	})
+	if (!accountLookup) {
+		return err({
+			message: 'Account could not be validated',
+			code: 404,
+		})
+	}
 	const depthChartLookup = await db.depthChart.findUnique({
 		where: {
 			uuid: params.depthChart,
 		},
 	})
 
-	if (account.uuid !== params.account) {
-		return err({
-			message: 'Account could not be validated',
-			code: 404,
-		})
-	}
 	if (!depthChartLookup) {
 		return err({
 			message: 'DepthChart could not be validated',
@@ -34,7 +36,7 @@ export const parseUnsubscribeLoader = async ({
 	}
 
 	return ok({
-		accountUUID: account.uuid,
+		accountUUID: accountLookup.uuid,
 		depthChartUUID: depthChartLookup.uuid,
 	})
 }
